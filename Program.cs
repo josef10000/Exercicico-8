1979cc2fbd07f682c089e51e1f8d00e28bc077a6
José Frazão <102549534+josef10000@users.noreply.github.com>
2023-04-04 15:32:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ler temperatura em Celsius
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+
+            // Converter para Fahrenheit
+            double fahrenheit = (9 * celsius + 160) / 5;
+
+            // Exibir resultado
+            Console.WriteLine("{0} graus Celsius equivalem a {1} graus Fahrenheit", celsius, fahrenheit);
+
+            Console.ReadKey();
+        }
+    }
+}
